a222510e6517d40a18a4ec245cc6f48a9d0334d7
tiennguyen00 <18521487@gm.uit.edu.vn>
2025-02-15 01:19:02 +0700

    fix touch range in mobile

--- components/MainPage.tsx
+++ components/MainPage.tsx
@@ -115,6 +115,7 @@ const MainPage = () => {
       const t = e.changedTouches[0];
       touch.current.dx = t.pageX - touch.current.startX;
       touch.current.dy = t.pageY - touch.current.startY;
+      if (touch.current.dy === 0) return;
       if (touch.current.dy > 10) direction.current = "up";
       if (touch.current.dy < -10) direction.current = "down";
       handleDirection();
