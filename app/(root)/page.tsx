5e2a4c45dcb9028b13fd6d4d38154bcb14574536
Tien Nguyen <nhtiendev@gmail.com>
2025-03-25 16:10:55 +0700

    fix: remove Suspense

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,5 @@
 import MainPage from "@/components/MainPage";
-import { Suspense } from "react";
 
 export default async function Home() {
-  return (
-    <Suspense>
-      <MainPage />
-    </Suspense>
-  );
+  return <MainPage />;
 }
